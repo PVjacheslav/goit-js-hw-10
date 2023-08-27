5717bfac15e4ff216feac2cd2a7454c5dd7db08c
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-08-27 19:36:26 +0300

    continue task

--- src/cat-api.js
+++ src/cat-api.js
@@ -9,6 +9,11 @@ function fetchBreeds() {
 
 function fetchCatByBreed(breedId) {
   const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+
+  return axios
+    .get(BASE_URL)
+    .then(response => response.data)
+    .catch(err => error(err));
 }
 
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -9,7 +9,31 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api';
 axios.defaults.headers.common['x-api-key'] =
   'live_SVj1aIeXMYYbjko4jsELVaTWh5G7QRZpDR6dEKTmG2xnihIaIjOmFSolCblWAzhx';
 
-elements.searchSelect.addEventListener('change', fetchBreeds);
+elements.searchSelect.addEventListener('change', handlerSearch);
 
-// const { url } = arr[0];
-// const { name, description, temperament } = arr[0].breeds[0];
+function handlerSearch(evt) {
+  fetchBreeds()
+    .then(data => (elements.container.innerHTML = createMarkup(data)))
+    .catch(err => console.log(err))
+    .finally(() => evt.target.reset());
+}
+
+function createMarkup() {
+  const { url } = arr[0];
+  const { name, description, temperament } = arr[0].breeds[0];
+  return;
+}
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ reference_image_id, name, description, temperament }) =>
+//         ` <li>
+//         <img src="${reference_image_id}.jpg" alt="${name}">
+//         <h2>${name}</h2>
+//         <p>${description}</p>
+//         <p>${temperament}</p>
+//       </li>`
+//     )
+//     .join('');
+// }
